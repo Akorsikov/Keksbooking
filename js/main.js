172c9befb30ebe870b664da662b5c622393fc8fe
Akorsikov <akors@mail.ru>
2023-04-23 21:33:46 +0300

    fix: behavior getRandomIntegerInRange

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@
 /**
  * Функция, возвращающая случайное целое число из переданного диапазона включительно
  * (диапазон может быть только положительный, включая ноль).
- * @param {integer} min integer - целое положительное число, начало диапазона(входит в диапазон);
- * @param {integer} max integer - целое положительное число, конец диапазона(входит в диапазон);
+ * @param {integer} range1 - целое положительное число, начало(или конец) диапазона(входит в диапазон);
+ * @param {integer} range2 - целое положительное число, конец(или начало) диапазона(входит в диапазон);
  * @return {integer} - случайное целое положительное число из переданного диапазона(включая min и max);
+ * @return {undefined} - undefined в случае не корректного диапазона.
  * */
-const getRandomIntegerInRange = function (min, max) {
-  if (min < 0 ||
-      max < 0 ||
-      min % 1 > 0 ||
-      max % 1 > 0 ||
-      min >= max) {
+const getRandomIntegerInRange = (range1, range2) => {
+  if (range1 < 0 ||
+      range2 < 0 ||
+      range1 % 1 > 0 ||
+      range2 % 1 > 0 ||
+      range1 === range2) {
     return undefined;
+  } else if (range1 > range2) {
+    [range1, range2] = [range2, range1];
   }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (range2 - range1 + 1)) + range1;
 }
-
