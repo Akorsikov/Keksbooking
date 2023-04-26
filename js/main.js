44a7b54a0ffd81af2ec0d6e8de457ae18fccbd05
Akorsikov <akors@mail.ru>
2023-04-26 11:02:36 +0300

    fix: return into getRandomFloatInRange

--- js/main.js
+++ js/main.js
@@ -19,3 +19,29 @@ const getRandomIntegerInRange = (range1, range2) => {
   }
   return Math.floor(Math.random() * (range2 - range1 + 1)) + range1;
 }
+
+// start of the project
+/**
+ * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+ * (диапазон может быть только положительный, включая ноль) с указанным "количеством знаков после запятой".
+ * @param {number} range1 - число с плавающей точкой, начало(или конец) диапазона(входит в диапазон);
+ * @param {number} range2 - число с плавающей точкой, конец(или начало) диапазона(входит в диапазон);
+ * @param {number} depthAfterDot - целое положительное число, с указывающее "количество знаков после запятой";
+ * @return {number} - случайное положительное число с плавающей точкой из переданного диапазона(включая range1 и range2);
+ * @return {undefined} - undefined в случае не корректных диапазона или количества знаков после запятой.
+ * */
+const getRandomFloatInRange = (range1, range2, depthAfterDot ) => {
+  if (range1 < 0 ||
+      range2 < 0 ||
+      depthAfterDot < 0 ||
+      depthAfterDot % 1 !== 0 ||
+      range1 === range2) {
+    return undefined;
+  } else if (range1 > range2) {
+    [range1, range2] = [range2, range1]
+  }
+  const tenToDegree = Math.pow(10, depthAfterDot);
+  const randomNumber = (Math.random() * (range2 - range1) + range1);
+  return Math.round(randomNumber * tenToDegree) / tenToDegree;
+}
+
