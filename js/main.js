e0fb33a6d699e024b1449c38c3454193cd049ade
Akorsikov <akors@mail.ru>
2023-10-17 11:21:55 +0300

    refactor: move call modules map.js, validation-form.js from index.html to main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 import {getArrayRandomAds} from './data.js';
 import {getAdCard} from './ad-card.js';
+import './map.js';
+import './validation-form.js';
 
-// L.map('map-canvas')
-//   .setView({
-//     lat: 59.92749,
-//     lng: 30.31127,
-//   }, 10);
-
-//const mapCanvas = document.querySelector('#map-canvas');
-//getMap(59.92749, 30.31127);//const ads = getArrayRandomAds();
-//const advertisement = getAdCard(ads[0]);
-//mapCanvas.appendChild(advertisement);
 
